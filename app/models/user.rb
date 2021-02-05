06e5444c88f7ad656416f4f23301820ac744c57c
Kelvin Lanier <kelvinlanier.com>
2021-02-05 15:23:10 -0500

    completed the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,3 +8,18 @@ class User < ApplicationRecord
     validates :email, uniqueness: true
 
     accepts_nested_attributes_for :wings
+
+    default_scope { order(:name) }
+
+    #scope method for session login omniauth
+    def self.find_or_create_by_omniauth(auth_hash)
+        self.where(:name => auth_hash['info']['name'], :email => auth_hash['info']['email']).first_or_create do |user|
+            user.password = SecureRandom.hex(16)
+        end
+    end
+
+    def self.user_search(u_search)
+        self.where("name LIKE ?", "%" + u_search + "%")
+    end
+
+end
