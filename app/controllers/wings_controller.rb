3b241796f012442c235746b9512114d277d846ef
Kelvin Lanier <kelvinlanier.com>
2021-02-05 17:07:05 -0500

    updated wings controller

--- app/controllers/wings_controller.rb
+++ app/controllers/wings_controller.rb
@@ -1,7 +1,7 @@
 class WingsController < ApplicationController
 
     before_action :redirect_if_not_logged_in
-    before_action :find_wing #use private method for finding game as a before action call
+    before_action :find_wing #use private method for finding wing as a before action call
 
     def new
         @wing = Wing.new(user_id: params[current_user.id])
@@ -9,8 +9,22 @@ class WingsController < ApplicationController
 
     def index
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
-            @wings = @user.games
+            @wings = @user.wings
         else
             @wings = Wing.all 
+        end
+    end
+
+    def show
+        @user = User.find_by_id(params[:user_id])
+    end
+
+    def create
+        @wing = current_user.wings.build(wing_params)
+
+        if @wing.save
+            redirect_to user_path(current_user)
+        else
+            render :new
         end
     end
