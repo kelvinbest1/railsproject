0563d803b2c1a139f4d2e5aad6ddeb9cb0bb1480
Kelvin Lanier <kelvinlanier.com>
2021-02-06 18:53:57 -0500

    updated session view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+ 
+  root 'sessions#new'
+  
+  get '/login' => 'sessions#new'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  post '/sessions' => 'sessions#create'
+  delete '/sessions/' => 'sessions#destroy'
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/search' => 'users#search'
