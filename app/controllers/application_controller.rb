8fc833a8e2ca5dc965fde8dabb99f78345d4e338
Kelvin Lanier <kelvinlanier.com>
2021-02-06 11:44:22 -0500

    updated SessionController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,3 +12,7 @@ class ApplicationController < ActionController::Base
         !!session[:user_id]
     end
 
+    def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in?
+    end
+end
