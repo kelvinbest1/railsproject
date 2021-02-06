f46b9d3a3128932e77a6419f302df4b76fa46248
Kelvin Lanier <kelvinlanier.com>
2021-02-06 10:53:07 -0500

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,3 +15,20 @@ class UsersController < ApplicationController
         @user = User.new
         @user.wings.build #nested attributes build for new view in user signup
     end
+
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+
+    def show
+        redirect_if_not_logged_in
+        @user = User.find_by_id(params[:id])
+    end
