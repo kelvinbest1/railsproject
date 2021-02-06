33d68c96de71111771b14ec203c671f402945e55
Kelvin Lanier <kelvinlanier.com>
2021-02-06 11:05:49 -0500

    finished session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,3 +10,20 @@ class SessionsController < ApplicationController
             @user = User.find_or_create_by_omniauth(auth_hash)
                 session[:user_id] = @user.id
                 redirect_to user_path(@user)
+            else
+                @user = User.find_by(email: params[:email])
+                if @user && @user.authenticate(params[:password])
+                    session[:user_id] = @user.id
+                    redirect_to user_path(@user)
+                else
+                    redirect_to login_path
+                end
+            end
+        end
+    
+        def destroy
+            session.clear
+            redirect_to login_path
+        end
+    
+    end
